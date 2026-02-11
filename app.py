d02d1149b4ef0030ad4ae847b189edbfa19781e3
Marty450 <kavrakovmartin3@gmail.com>
2026-02-11 14:18:02 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Тест по География")
+
+st.write("Коя е столицата на Япония?")
+user_answer1=st.radio("Вашия отговор:"["Токио","Осака","Хипошима","Франция"]
+if st.button("Провери отговор 1")
+                      if user answer 1.strip().lower() =="Токио":st.succes("Верен отговор")
+                      else:st.error("Столицата на япония е Токио")
+
+
